fe6a1761dd72a3ee0c36c9fb51acd035fc97c369
Richard Samuels <richard.samuels@mongodb.com>
2018-03-13 15:40:46 -0400

    fix ignore unresolved symbols on linux

--- lib.go
+++ lib.go
@@ -1,7 +1,7 @@
 package main
 
 // #cgo darwin LDFLAGS: -Wl,-U,_InsertHighlight,-U,_Errored
-// #cgo linux LDFLAGS: -Wl,-u,InsertHighlight,-u,Errored
+// #cgo linux LDFLAGS: -Wl,--unresolved-symbols=report-all
 // #include "golang.h"
 import "C"
 
