ac9cb998979fd0201011f1d7fd2e9b4d91e2f5e2
Richard Samuels <richard.samuels@mongodb.com>
2018-03-13 12:10:54 -0400

    use real golang.h

--- .build/golang.h
+++ .build/golang.h
@@ -1,5 +1,21 @@
-struct Callback {
+#pragma once
 
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct Callback {
+    void *data_ptr;
+    void *err_str;
 };
+
+void InsertHighlight(struct Callback c, char * type, int line, int column,
+        char * token);
+
 void Errored(struct Callback c, char* msg);
-void InsertHighlight(struct Callback c, char * type, int line, int column, char* token);
+
+#ifdef __cplusplus
+}
+#endif
